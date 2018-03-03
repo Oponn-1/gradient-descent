1879648d8f2588bea868d888c1fb952191e271d9
Andres De La Fuente Duran <33918646+Oponn-1@users.noreply.github.com>
2018-03-03 09:18:03 -0500

    added starter files

---
+++ homework2.py
@@ -0,0 +1,10 @@
+
+import math
+import random
+import numpy as npdef 
+
+bgd_l2(data, y, w, eta, delta, lam, num_iter):
+	return new_w, history_fwdef 
+
+sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
+	return new_w, history_fw
