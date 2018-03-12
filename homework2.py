5391de9eb590bde84959ba1f6a59586a1c5083f7
Andres De La Fuente Duran <33918646+Oponn-1@users.noreply.github.com>
2018-03-11 20:33:17 -0400

    Added implementations of 4 functions
    implemented in full:
    batch gradient descent
    stochastic gradient descent
    objective function
    gradient function

--- homework2.py
+++ homework2.py
@@ -1,10 +1,67 @@
 
 import math
 import random
-import numpy as npdef 
+import numpy as np 
 
-bgd_l2(data, y, w, eta, delta, lam, num_iter):
-	return new_w, history_fwdef 
+def gradient(x, y, w, delta, lam):
+	gradient = [0] * len(w)
+	for n in range(len(x)):
+		if (y[n] >= np.dot(w, x[n]) + delta):
+			for n in range(len(x)):
+				gradient += np.multiply((-2 * (y[n] - np.dot(w, x[n]) - delta)), (x[n]))
+			gradient = gradient / len(x)
+		elif (y[n] <= np.dot(w, x[n]) - delta):
+			for n in range(len(x)):
+				gradient += np.multiply((-2 * (y[n] - np.dot(w, x[n]) + delta)), (x[n]))
+			gradient = gradient / len(x)
+	regularization = 0
+	for d in range(len(w)):
+		regularization += w[d]
+	gradient += lam * 2 * regularization
+	return gradient
 
-sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
-	return new_w, history_fw
+def objective(x, y, w, delta, lam):
+	objective = [0] * len(w)
+	for n in range(len(x)):
+		if (y[n] >= np.dot(w, x[n]) + delta):
+			for n in range(len(x)):
+				objective += ((y[n] - np.dot(w, x[n]) - delta) ** 2)
+			objective = objective / len(x)
+		elif (y[n] <= np.dot(w, x[n]) - delta):
+			for n in range(len(x)):
+				objective += ((y[n] - np.dot(w, x[n]) + delta) ** 2)
+			objective = objective / len(x)
+	regularization = 0
+	for d in range(len(w)):
+		regularization += w[d] ** 2
+	objective += lam * regularization
+	return objective
+
+def bgd_l2(data, y, w, eta, delta, lam, num_iter):
+	history_fwdef = []
+	this_w = w
+	for i in range(0, num_iter):
+		new_w = this_w - eta*gradient(data, y, this_w, delta, lam)
+		history_fwdef.append(objective(data, y, this_w, delta, lam))
+		this_w = new_w
+	return this_w, history_fwdef 
+
+def sgd_l2(data, y, w, eta, delta, lam, num_iter, i=-1):
+	history_fw = []
+	this_w = w
+	if (i == -1):
+		for n in range(0, num_iter):
+			selection = random.randint(0, len(data) -1)
+			x_selection = [data[selection]]
+			y_selection = [y[selection]]
+			new_w = this_w - (eta / math.sqrt(n+1))*gradient(x_selection, y_selection, this_w, delta, lam)
+			history_fw.append(objective(x_selection, y_selection, this_w, delta, lam))
+			this_w = new_w
+	else:
+		selection = i
+		x_selection = [data[selection]]
+		y_selection = [y[selection]]
+		new_w = this_w - (eta / math.sqrt(n+1))*gradient(x_selection, y_selection, this_w, delta, lam)
+		history_fw.append(objective(x_selection, y_selection, this_w, delta, lam))
+		this_w = new_w
+	return this_w, history_fw
